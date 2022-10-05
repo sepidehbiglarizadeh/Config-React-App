1baba813ec30bfd09c450699c629c3b84e0df538
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-10-05 17:33:35 +0330

    add css and file loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import './App.css';
+import logo from './logo.png'
 
 const App = () => {
     return (
-        <h1>Welcome To React Application</h1>
+        <div className='App'>
+            <h1>Welcome To React Application</h1>
+            <img src={logo} alt="logo"/>
+        </div>
       );
 }
  
